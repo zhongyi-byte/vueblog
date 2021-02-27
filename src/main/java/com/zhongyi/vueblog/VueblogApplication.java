2c5341baec6d039946117c66b210a91eea0a37c6
zhongyi-byte <62290010+zhongyi-byte@users.noreply.github.com>
2021-02-27 09:34:43 +0800

    vueblog init
    创建了基本框架

---
+++ src/main/java/com/zhongyi/vueblog/VueblogApplication.java
@@ -0,0 +1,13 @@
+package com.zhongyi.vueblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueblogApplication.class, args);
+	}
+
+}
